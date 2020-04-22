0bd917b6482c8c2095b322de7f82867e263eedcd
byshy <baselhadrous99@gmail.com>
2020-04-22 09:07:05 +0300

    minor refactoring

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from api.views import BloodCenterListView
 
 urlpatterns = [
-    path('blood_centers/', BloodCenterListView.as_view(), name='get_blood_centers'),                                # add a new project
-    path('auth/', include('rest_auth.urls')),       # DRF auth system
+    path('blood_centers/', BloodCenterListView.as_view(), name='get_blood_centers'),
+    path('auth/', include('rest_auth.urls')),   # DRF auth system
 ]
