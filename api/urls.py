2ba3ce8298c01bbbe91d90f5a7400fb0f98f8db2
byshy <baselhadrous99@gmail.com>
2020-04-22 12:15:04 +0300

    get a donor based on their ID number

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from api.views import BloodCenterListView, BloodCenterView
+from api.views import BloodCenterListView, BloodCenterView, DonorView
 
 urlpatterns = [
     path('blood_centers/', BloodCenterListView.as_view(), name='get_blood_centers'),
-    path('blood_center/<str:id>', BloodCenterView.as_view(), name='get_blood_center'),
+    path('blood_center/<str:name>', BloodCenterView.as_view(), name='get_blood_center'),
+    path('donor/<int:id>', DonorView.as_view(), name='get_donor'),
     path('auth/', include('rest_auth.urls')),   # DRF auth system
 ]
--- api/views.py
+++ api/views.py
@@ -5,9 +5,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import BloodCenter
+from api.models import BloodCenter, Donor
 from api.permissions import ReadOnly
-from api.serializers import BloodCenterSerializer
+from api.serializers import BloodCenterSerializer, DonorSerializer
 
 
 class BloodCenterListView(generics.ListAPIView):
@@ -21,16 +21,40 @@ class BloodCenterView(APIView):
     http_method_names = ['get']
     permission_classes = [IsAuthenticated]
 
+    def get_object(self, name):
+        try:
+            return BloodCenter.objects.get(name=name)
+        except BloodCenter.DoesNotExist:
+            raise Http404
+
+    def get(self, request, name):
+        try:
+            center = self.get_object(name)
+            serializer = BloodCenterSerializer(center)
+            content = {
+                'data': serializer.data,
+                'msg': 'element retrieved successfully',
+                'status': status.HTTP_200_OK
+            }
+            return Response(content)
+        except Http404:
+            return Response({'msg': 'element not found', 'status': status.HTTP_404_NOT_FOUND})
+
+
+class DonorView(APIView):
+    http_method_names = ['get']
+    permission_classes = [IsAuthenticated]
+
     def get_object(self, id):
         try:
-            return BloodCenter.objects.get(name=id)
-        except BloodCenter.DoesNotExist:
+            return Donor.objects.get(id=id)
+        except Donor.DoesNotExist:
             raise Http404
 
     def get(self, request, id):
         try:
-            pp = self.get_object(id)
-            serializer = BloodCenterSerializer(pp)
+            donor = self.get_object(id)
+            serializer = DonorSerializer(donor)
             content = {
                 'data': serializer.data,
                 'msg': 'element retrieved successfully',
